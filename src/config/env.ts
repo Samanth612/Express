22f061bd694c3979a08719aeaea91dcaefbb395b
Samanth612 <rocketspeed35@gmail.com>
2025-02-07 13:14:14 +0530

    Fix .gitignore for Elastic Beanstalk deployment

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 8080,
   mongoURI: process.env.MONGO_URI as string,
   jwtSecret: process.env.JWT_SECRET as string,
 };
