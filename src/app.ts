340ce773462b9cc1a5bfe65249859e8b32083538
Samanth612 <rocketspeed35@gmail.com>
2025-02-07 17:05:39 +0530

    .gitignore updated

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import todoListRoutes from "./routes/todoList.routes";
 import { sendResponse } from "./utils/response";
+import { todoList as List } from "./models/todoList.model";
+import path from "path";
 
 const app = express();
 
@@ -12,6 +14,18 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 
 app.use("/api/todos", todoListRoutes);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", async (req, res) => {
+  try {
+    const todos = await List.find({});
+    res.render("todo", { todos });
+  } catch (error) {
+    sendResponse(res, 500, false, "Error fetching TodoList", error);
+  }
+});
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.log("Error:", err.message);
