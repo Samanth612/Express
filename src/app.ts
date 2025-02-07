c4fc469bda6596d5aa696768ccae637653a6e62a
Samanth612 <rocketspeed35@gmail.com>
2025-02-07 10:58:58 +0530

    Added centralized error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import todoListRoutes from "./routes/todoList.routes";
+import { sendResponse } from "./utils/response";
 
 const app = express();
 
@@ -11,5 +12,15 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 
 app.use("/api/todos", todoListRoutes);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log("Error:", err.message);
+  sendResponse(
+    res,
+    err.status || 500,
+    false,
+    err.message || "Internal Server Error"
+  );
+});
 
 export default app;
